1df57aa701e8c2b65031d0d5a154d9f716e1717f
Liao Yan <liaoxueyan030106@gmail.com>
2024-05-07 21:50:31 +0800

    test

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helloworld");
-        System.out.println("helloworld2");
+        System.out.println("helloworld1");
         System.out.println("helloworld3");
     }
 }
